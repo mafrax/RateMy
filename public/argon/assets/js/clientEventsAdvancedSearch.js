7fed74cc88a8053d7f0b77b4ab3afa080e130f74
mafrax <mongin.marc@gmail.com>
2019-08-15 18:09:46 +0200

    test

--- public/argon/assets/js/clientEventsAdvancedSearch.js
+++ public/argon/assets/js/clientEventsAdvancedSearch.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http:///localhost:3000/advancedSearch')
+var socket = io.connect('http:///rateyourporn.com/advancedSearch')
 
 socket.on('truc', function (message) {
     console.log(message)
