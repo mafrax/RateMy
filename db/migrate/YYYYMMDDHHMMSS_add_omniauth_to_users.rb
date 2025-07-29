f3b28030e33549ddd3f496e2a323f35e94253873
Marc <31855433+mafrax@users.noreply.github.com>
2025-07-29 14:25:42 +0300

    Implement: Add google, facebook , apple login

--- db/migrate/YYYYMMDDHHMMSS_add_omniauth_to_users.rb
+++ db/migrate/YYYYMMDDHHMMSS_add_omniauth_to_users.rb
@@ -1,9 +1,1 @@
-# db/migrate/YYYYMMDDHHMMSS_add_omniauth_to_users.rb
-class AddOmniauthToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :image, :string
-    add_index :users, [:provider, :uid], unique: true
-  end
-end
+// Content will be retrieved from GitHub or generated
